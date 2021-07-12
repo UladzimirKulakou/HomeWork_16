fd6a096380d1366eb265a9b5190698a8863b5dfa
UladzimirKulakou <rombik9@ya.ru>
2021-07-12 14:18:08 +0300

    commit 45

--- HomeWork_16/HomeWork_16/Controllers/ColorChangeVC.swift
+++ HomeWork_16/HomeWork_16/Controllers/ColorChangeVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ColorChangeVC: UIViewController {
-    
+
     var complition: ((UIColor, Float) -> ())?
 
     @IBOutlet weak var redSlider: UISlider!
@@ -23,7 +23,7 @@ class ColorChangeVC: UIViewController {
     @IBOutlet weak var opacityTF: UITextField!
     @IBOutlet weak var opacitySlider: UISlider!
     @IBOutlet weak var downView: UIView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -40,115 +40,104 @@ class ColorChangeVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    func hexFromUIColor(color: UIColor) -> String
-    {
-        let hexString = String(format: "%02X%02X%02X",
-            Int((color.cgColor.components?[0])! * 255.0),
-            Int((color.cgColor.components?[1])! * 255.0),
-            Int(color.cgColor.components![2] * 255.0))
-        return hexString
-    }
+
 
 
     @IBAction func redTFAction(_ sender: Any) {
-        guard let valueRed = redTF.text else { return }
-        guard let number = Float(valueRed) else { return }
+        let number = TextToSlider().textToNumber(text: redTF.text)
         (number >= 0 && number <= 255) ? (redSlider.value = number) : (redSlider.value = 255)
         (number >= 0 && number <= 255) ? (redSlider.value = number) : (redTF.text = "255")
-        downView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255,
-            green: CGFloat(greenSlider.value) / 255,
-            blue: CGFloat(blueSlider.value) / 255,
-            alpha: CGFloat(opacitySlider.value / 100))
-        hexTF.text = hexFromUIColor(color: downView.backgroundColor!)
+        downView.backgroundColor = UpdateDownViewColor.updateDownViewColor(red: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value,
+            opacity: opacitySlider.value)
+        hexTF.text = ConvertColor().hexFromUIColor(color: downView.backgroundColor!)
 
     }
 
     @IBAction func greenTFAction(_ sender: Any) {
-        guard let valueRed = greenTF.text else { return }
-        guard let number = Float(valueRed) else { return }
+        let number = TextToSlider().textToNumber(text: greenTF.text)
         (number >= 0 && number <= 255) ? (greenSlider.value = number) : (greenSlider.value = 255)
         (number >= 0 && number <= 255) ? (greenSlider.value = number) : (greenTF.text = "255")
-        downView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255,
-            green: CGFloat(greenSlider.value) / 255,
-            blue: CGFloat(blueSlider.value) / 255,
-            alpha: CGFloat(opacitySlider.value / 100))
-        hexTF.text = hexFromUIColor(color: downView.backgroundColor!)
+        downView.backgroundColor = UpdateDownViewColor.updateDownViewColor(red: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value,
+            opacity: opacitySlider.value)
+        hexTF.text = ConvertColor().hexFromUIColor(color: downView.backgroundColor!)
 
     }
     @IBAction func blueTFActiob(_ sender: Any) {
-        guard let valueRed = blueTF.text else { return }
-        guard let number = Float(valueRed) else { return }
+        let number = TextToSlider().textToNumber(text: blueTF.text)
         (number >= 0 && number <= 255) ? (blueSlider.value = number) : (blueSlider.value = 255)
         (number >= 0 && number <= 255) ? (blueSlider.value = number) : (blueTF.text = "255")
-        downView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255,
-            green: CGFloat(greenSlider.value) / 255,
-            blue: CGFloat(blueSlider.value) / 255,
-            alpha: CGFloat(opacitySlider.value / 100))
-        hexTF.text = hexFromUIColor(color: downView.backgroundColor!)
+        downView.backgroundColor = UpdateDownViewColor.updateDownViewColor(red: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value,
+            opacity: opacitySlider.value)
+        hexTF.text = ConvertColor().hexFromUIColor(color: downView.backgroundColor!)
 
     }
     @IBAction func opacituTFAction(_ sender: Any) {
-        guard let valueRed = opacityTF.text else { return }
-        guard let number = Float(valueRed) else { return }
+        let number = TextToSlider().textToNumber(text: opacityTF.text)
         (number >= 0 && number <= 100) ? (opacitySlider.value = number) : (opacitySlider.value = 100)
         (number >= 0 && number <= 100) ? (opacitySlider.value = number) : (opacityTF.text = "100")
-        downView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255,
-            green: CGFloat(greenSlider.value) / 255,
-            blue: CGFloat(blueSlider.value) / 255,
-            alpha: CGFloat(opacitySlider.value / 100))
-        hexTF.text = hexFromUIColor(color: downView.backgroundColor!)
+        downView.backgroundColor = UpdateDownViewColor.updateDownViewColor(red: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value,
+            opacity: opacitySlider.value)
+        hexTF.text = ConvertColor().hexFromUIColor(color: downView.backgroundColor!)
 
     }
 
-    
+
 
     @IBAction func redSliderAction(_ sender: Any) {
 //        guard let valueRed = redTF.text else { return }
 //        guard let number = Int(valueRed) else { return }
 
         redTF.text = String(Int(redSlider.value))
-        downView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255,
-            green: CGFloat(greenSlider.value) / 255,
-            blue: CGFloat(blueSlider.value) / 255,
-            alpha: CGFloat(opacitySlider.value / 100))
-        hexTF.text = hexFromUIColor(color: downView.backgroundColor!)
+        downView.backgroundColor = UpdateDownViewColor.updateDownViewColor(red: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value,
+            opacity: opacitySlider.value)
+        hexTF.text = ConvertColor().hexFromUIColor(color: downView.backgroundColor!)
 
     }
     @IBAction func greenSliderAction(_ sender: Any) {
         greenTF.text = String(Int(greenSlider.value))
-        downView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255,
-            green: CGFloat(greenSlider.value) / 255,
-            blue: CGFloat(blueSlider.value) / 255,
-            alpha: CGFloat(opacitySlider.value / 100))
-        hexTF.text = hexFromUIColor(color: downView.backgroundColor!)
+        downView.backgroundColor = UpdateDownViewColor.updateDownViewColor(red: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value,
+            opacity: opacitySlider.value)
+        hexTF.text = ConvertColor().hexFromUIColor(color: downView.backgroundColor!)
     }
     @IBAction func blueSliderAction(_ sender: Any) {
         blueTF.text = String(Int(blueSlider.value))
-        downView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255,
-            green: CGFloat(greenSlider.value) / 255,
-            blue: CGFloat(blueSlider.value) / 255,
-            alpha: CGFloat(opacitySlider.value / 100))
-        hexTF.text = hexFromUIColor(color: downView.backgroundColor!)
+        downView.backgroundColor = UpdateDownViewColor.updateDownViewColor(red: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value,
+            opacity: opacitySlider.value)
+        hexTF.text = ConvertColor().hexFromUIColor(color: downView.backgroundColor!)
     }
     @IBAction func opacitySliderAction(_ sender: Any) {
         opacityTF.text = String(Int(opacitySlider.value))
-        downView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255,
-            green: CGFloat(greenSlider.value) / 255,
-            blue: CGFloat(blueSlider.value) / 255,
-            alpha: CGFloat(opacitySlider.value / 100))
-        hexTF.text = hexFromUIColor(color: downView.backgroundColor!)
+        downView.backgroundColor = UpdateDownViewColor.updateDownViewColor(red: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value,
+            opacity: opacitySlider.value)
+        hexTF.text = ConvertColor().hexFromUIColor(color: downView.backgroundColor!)
     }
-    
-    
-  
+
+
+
     @IBAction func buttonTapped() {
         guard let color = downView.backgroundColor else { return }
-          let opacity = opacitySlider.value / 100
+        let opacity = opacitySlider.value / 100
         complition?(color, opacity)
         _ = navigationController?.popToRootViewController(animated: true)
         print(opacity)
     }
-    
+
 
 
 }
--- HomeWork_16/HomeWork_16/Resources/HexFromUIColor.swift
+++ HomeWork_16/HomeWork_16/Resources/HexFromUIColor.swift
@@ -10,5 +10,12 @@ import Foundation
 import UIKit
 
 class ConvertColor {
-
+    func hexFromUIColor(color: UIColor) -> String
+    {
+        let hexString = String(format: "%02X%02X%02X",
+            Int((color.cgColor.components?[0])! * 255.0),
+            Int((color.cgColor.components?[1])! * 255.0),
+            Int(color.cgColor.components![2] * 255.0))
+        return hexString
+    }
 }
